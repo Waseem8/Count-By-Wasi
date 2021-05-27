e5fd98395e06e3fd37d5b0512b2d3b20fe519eca
Waseem <wasirana80@gmail.com>
2021-05-27 12:41:05 +0500

    style added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Image from './Image/countdown.svg'
 import Time from './components/Timer'
 import './App.css'
+import { motion } from "framer-motion"
+
+
 function App() {
   return (
     <div class="coming-soon">
@@ -8,7 +11,13 @@ function App() {
             <h2>We are opening soon!</h2>
             <Time/>
             </div>
-        <img className="waiting" src={Image} alt=""/>
+        <motion.img className="waiting" src={Image} alt="" drag
+    dragConstraints={{
+      top: 0,
+      left: -1000,
+      right: 1000,
+      bottom: 10,
+    }}/>
     </div>
   );
 }
